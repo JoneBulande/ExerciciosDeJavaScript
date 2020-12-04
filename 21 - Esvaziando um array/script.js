04d41507e5839de1144a8cc1c205185673cbdee0
JoneBulande <JoneBulande@gmail.com>
2020-12-04 10:21:21 +0200

    esvaziar array

---
+++ 21 - Esvaziando um array/script.js
@@ -0,0 +1,23 @@
+// esvaziar um array
+let numeros = [1,2,3,4,5]
+
+// Solução 1
+// console.log(numeros)
+// const outrosNs = numeros;
+// console.log(outrosNs)
+//numeros = [];
+
+// Solução 2
+// console.log(numeros)
+// numeros.length = 0;
+// console.log(numeros)
+
+// Solução 3
+// console.log(numeros)
+// numeros.splice(0,numeros.length);
+// console.log(numeros)
+
+// Solução 4
+// while (numeros.length > 0) {
+// 	numeros.pop();
+// }
