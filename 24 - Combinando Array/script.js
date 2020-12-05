71eac7bd8213e94524da17d9a26fb4812823d96e
JoneBulande <JoneBulande@gmail.com>
2020-12-06 00:13:25 +0200

    Exercício 24 - Combinando Arrays - trasformando Arrays em string, string em arrays, slug

---
+++ 24 - Combinando Array/script.js
@@ -0,0 +1,13 @@
+const numeros = [1,2,3]
+
+// transformar Array em String
+const str1 = numeros.join(' ')
+console.log(str1)
+
+// Transformar String em Array
+const num1 = str1.split(' ')
+console.log(num1)
+
+// slug 
+const slug1 = num1.join('-');
+console.log(slug1)
